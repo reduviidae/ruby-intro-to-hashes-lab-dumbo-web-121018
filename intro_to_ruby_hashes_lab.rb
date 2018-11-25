f50b8034a4420d301da92ce341510c02439a3706
Wil Cook <wil.cook.is@gmail.com>
2018-11-25 16:16:42 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,8 @@ def monopoly_with_second_tier
 	monopoly = {:railroads => {
 	  :pieces => 4, 
 	  :names => {},
-	  :rent_in_dollars => {
-	    :one_piece_owned => 25,
-	  }
+	  :rent_in_dollars => {},
+	  :one_piece_owned => 25
 	  }
 	}	
 end
